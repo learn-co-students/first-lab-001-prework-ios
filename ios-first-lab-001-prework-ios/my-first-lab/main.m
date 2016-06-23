15181ac47b599448ada8f63e24e515de6fed41b7
Ben <bennalle1@gmail.com>
2016-06-23 17:38:41 -0400

    commiting lab for second time

---
+++ ios-first-lab-001-prework-ios/my-first-lab/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  my-first-lab
+//
+//  Created by Joe Burgess on 5/4/15.
+//  Copyright (c) 2015 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FISAppDelegate class]));
+    }
+}
